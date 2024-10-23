1126cb68410ec7e9a6093ac924f70f648658a104
Enforsix <christian.gulkoder@gmail.com>
2024-10-23 11:18:09 +0200

    con log

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+console.log("Hello!");
+// variabel "let" kan! endre 
+// variabel "const" er konstatn/permanent
+
+// Let variabel named age saving a number 38 as data
+let age = 38;
+console.log(age);
+
+// Const variabel named "name" saving a string Christian as data
+const fname = "Christian";
+console.log(fname);
+
+// const variable Named isPresent saving a boolean true as data
+const isPresent = true;
+console.log(isPresent)
